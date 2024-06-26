be3cd69410bd56bdf70113902dff3d0eb1159256
harisaini778 <harikumarsaini778@gmail.com>
2024-06-26 17:20:36 +0530

    updated the endpoints

--- src/app/store/Features/slices/datastore.js
+++ src/app/store/Features/slices/datastore.js
@@ -16,7 +16,7 @@ export const fetchAllMeetUps = createAsyncThunk(
   'slices/fetchAllMeetUps',
   async (_, thunkAPI) => {
     try {
-      const response = await fetch('next-js-meet-up-two.vercel.app/api/FetchMeetUps');
+      const response = await fetch('https://next-js-meet-up-two.vercel.app/api/FetchMeetUps');
       
       if (!response.ok) {
         throw new Error("Failed to fetch meetups");
